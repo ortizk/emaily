93e7a960a63147e24c9b60cd982975cc5dcb01f4
ortizk <ortizk@live.com>
2018-07-06 15:42:37 -0700

    finished oauth

--- services/passport.js
+++ services/passport.js
@@ -1,10 +1,26 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 
-// this sends the user to google to give permission for my site to use their information then sends them back to the callbackurl which is my site
+const User = mongoose.model('users');
 
+
+//for cookies 
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id)
+	.then(user => {
+		done(null, user);
+	});
+});
+
+// this sends the user to google to give permission for my site to use their information then sends them back to the callbackurl which is my site
+// if you would like to use another strategy, wire it up here and also change the routes to match.
 passport.use(
 	new GoogleStrategy(
 	{
@@ -13,10 +29,20 @@ passport.use(
 		callbackURL: '/auth/google/callback'
 	}, 
 	(accessToken, refreshToken, profile, done) => {
-		console.log('access token: ', accessToken);
-		console.log('refreshToken: ', refreshToken);
-		console.log('profile: ', profile);
-		}
+		// we have to check if the user exists in db so that it doesn't add twice
+		User.findOne({ googleId: profile.id })
+			.then((existingUser) => {
+				if (existingUser) {
+					// we already have a record with the given Profile Id
+					done(null, existingUser);
+				} else {
+					// we don't have a user record with this ID, make a new record
+					new User({ googleId: profile.id }).save()
+						.then(user => done(null, user));
+				}
+			})
+		
+	}
 	)
 );
 
