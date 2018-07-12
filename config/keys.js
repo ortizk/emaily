251fd476c33ee002ac71f83c9e10b225f02ef963
ortizk <ortizk@live.com>
2018-07-11 18:11:52 -0700

    testing again

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,9 @@
 if (process.env.NODE_ENV === 'production'){
+	console.log('Prod Load')
 	// we are in production = return the prod set of keys
 	module.exports = require('./prod');
 } else {
+	console.log('dev Load')
 	// we are in development = return the dev keys
 	module.exports = require('./dev');
 }
