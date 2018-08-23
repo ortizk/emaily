7801562b63f9b758fe1d9effbfaac732d21dc6a1
ortizk <ortizk@live.com>
2018-08-23 14:50:20 -0700

    nm

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,12 +15,12 @@ module.exports = (app) => {
 	app.get('/api/logout', (req, res) => {
 		// logout is a function provided by passport. 
 		// takes the cookie and kills the id in it
+		console.log(res);
 		req.logout();
 		res.redirect('/')
 	});
 
 	app.get('/api/current_user', (req, res) => {
 		res.send(req.user);
-		// res.send('this guy');
 	});
 };
